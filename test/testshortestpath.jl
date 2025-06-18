b4daa60dd61f80fae6387f901bb8afde89a9bccc
jbytecode <mhsatman@gmail.com>
2025-06-18 22:02:55 +0300

    add test for shortestpath

--- test/testshortestpath.jl
+++ test/testshortestpath.jl
@@ -27,4 +27,44 @@
             @test element in CorrectShortestPath
         end
     end
+
+
+    @testset "Simple Problem 2" begin 
+        problem = ShortestPathProblem(
+            Connection[
+                Connection(1, 2, 2),
+                Connection(1, 3, 3),
+                Connection(1, 4, 4),
+                Connection(2, 3, 1),
+                Connection(3, 4, 1),
+                Connection(2, 5, 4),
+                Connection(3, 7, 2),
+                Connection(4, 6, 3),
+                Connection(6, 7, 1),
+                Connection(5, 7, 3),
+                Connection(5, 8, 8),
+                Connection(6, 9, 7),
+                Connection(8, 10, 5),
+                Connection(9, 10, 6),
+                Connection(7, 10, 10),
+            ]
+        )
+
+        # Shortest path: 1 -> 3 -> 7 -> 10
+        CorrectShortestPath = [
+            problem.connections[2], 
+            problem.connections[7], 
+            problem.connections[15]]
+
+        result = solve(problem)
+
+        display(result)
+        @test result isa ShortestPathResult
+        @test result.cost == 15.0
+
+        for element in result.path
+            @test element in CorrectShortestPath
+        end
+        
+    end 
 end
