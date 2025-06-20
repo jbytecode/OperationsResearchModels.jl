9e36c4df7062b948c0319255e59c713b859a7541
jbytecode <mhsatman@gmail.com>
2025-06-20 21:36:21 +0300

    change the order of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,13 @@ using OperationsResearchModels
 using OperationsResearchModels.Simplex
 
 
+include("testjump.jl")
+include("testassignment.jl")
 include("testtransportation.jl")
 include("testrandomkeyga.jl")
 include("testtravelingsalesman.jl")
 include("testjohnsons.jl")
 include("testutility.jl")
-include("testjump.jl")
-include("testassignment.jl")
 include("testshortestpath.jl")
 include("testmaximumflow.jl")
 include("testgame.jl")
